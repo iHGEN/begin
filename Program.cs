a3eaa981f620057292a2260a2c5400327515f80d
Hatem <64999740+iHGEN@users.noreply.github.com>
2023-08-29 17:50:43 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Into;
+
+namespace begin
+{
+    class Program
+   {
+        static void Main(string[] args)
+        {
+        fromstart:
+            Console.WriteLine("Enter first number");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("choose the condtion + or - or / or *");
+            string condtion = Console.ReadLine();
+            Console.WriteLine("Enter second number");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            if(condtion == "*")
+            {
+                Console.WriteLine(string.Concat("The result is : ",num1 * num2));
+            } else if(condtion == "/")
+            {
+                Console.WriteLine(string.Concat("The result is : ", num1 / num2));
+            } else if(condtion == "+")
+            {
+                Console.WriteLine(string.Concat("The result is : ", num1 + num2));
+            } else if(condtion == "-")
+            {
+                Console.WriteLine(string.Concat("The result is : ", num1 - num2));
+            }
+            else
+            {
+                Console.WriteLine("No condtion was found :)\r\n");
+            }
+            Console.WriteLine("Again ?");
+            if(Console.ReadLine().ToLower() == "y")
+            {
+                Console.Clear();
+                goto fromstart;
+            }
+        }
+    }
+}
---
+++ into.cs
@@ -0,0 +1,19 @@
+﻿
+
+namespace Into
+{
+    class into
+    {
+        int num1 = 0;
+        int num2 = 1;
+        public  into(int num1,int num2)
+        {
+            this.num1 = num1;
+            this.num2 = num2;
+        }
+        public int GetResult()
+        {
+            return num1 * num2;
+        }
+    }
+}
